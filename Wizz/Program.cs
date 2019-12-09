532de0d8d0df65dd81508f6efa13e215f92b3a97
Jacob Kjærgaard <ziro776@gmail.com>
2019-12-09 14:53:29 +0100

    Add project files.

---
+++ Wizz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
